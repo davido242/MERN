1702624931b8ef7ba989f009f7a477020e6e83dc
Monday_David_Sarka <david_md@hamari.agency>
2023-06-20 00:29:19 +0100

    Populated template files

--- src/index.js
+++ src/index.js
@@ -4,12 +4,21 @@ const app = express();
 const path = require("path");
 const hbs = require("hbs");
 
+const templatePath = path.join(__dirname, "../templates")
+
 app.use(express.json());
+app.set("view engine", "hbs");
+app.set("views", templatePath);
+
 
 
 app.get("/", (req, res) => {
     res.render("login");
-    console.log(("hi Daveedo"));
+    console.log(("My Login Page Loaded"));
+})
+app.get("/signup", (req, res) => {
+    res.render("signup");
+    console.log(("My Signup Page Loaded"));
 })
 
 app.listen(3000, () => {
